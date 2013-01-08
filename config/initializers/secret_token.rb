bb0d2b5eb4858be4a70053c881dd7be8a0a5d60d
Yas Pas <yrazvan@gmail.com>
2013-01-07 20:36:03 -0500

    initial commit. clean application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fyef::Application.config.secret_token = '236ad538189a983213b686525b2acf3195e8dc8c32ef8d9198fab7faea2449b10d56fa5d142342a8651f01f59634714ad87f5efcda95b76227b4c7acacdfdb97'
